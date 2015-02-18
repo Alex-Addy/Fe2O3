d002550b8add7d9987d126c6436fa7ba2e876a4e
Alex Addy <alex.addy@gmail.com>
2015-02-18 08:53:26 -0500

    Fixed printing values leftover in buf

--- net_test.rs
+++ net_test.rs
@@ -30,10 +30,10 @@ fn main() {
 	stream.write_fmt(format_args!("{} {}{}\r\n", "USER", nick, " 0 * :tutorial bot"));
 	stream.write_fmt(format_args!("{} {}\r\n", "JOIN", chan));
 
-	let mut buf = [0; 128];
+	let mut buf = [0; 64];
 	let mut bytes_read = stream.read(&mut buf).unwrap();
 	while bytes_read > 0 {
-		print!("{}", match std::str::from_utf8(&buf) {
+		print!("{}", match std::str::from_utf8(&buf[..bytes_read]) {
 			Ok(s) => s,
 			Err(e) => match e {
 				std::str::Utf8Error::InvalidByte(_) => "Invalid Byte",
