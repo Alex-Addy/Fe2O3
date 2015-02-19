230c9830969ef76550362e93d5159860ae418377
Alex Addy <alex.addy@gmail.com>
2015-02-18 21:09:11 -0500

    Added some test stuff and debug assist

--- net_test.rs
+++ net_test.rs
@@ -7,6 +7,11 @@ use std::io::Read;
 use std::io::Result;
 use std::io::Write;
 
+struct TcpWrapper {
+	stream: net::TcpStream,
+	buf: [u8],
+}
+
 fn start_connection(host: &str, port: u16) -> Result<net::TcpStream> {
 	let mut res = try!(net::lookup_host(host));
 	let first = try!(res.next().unwrap());
@@ -16,6 +21,14 @@ fn start_connection(host: &str, port: u16) -> Result<net::TcpStream> {
 	try!(stream.set_nodelay(true));
 
 	return Ok(stream);
+}
+
+fn send_line(mut stream: net::TcpStream, line: &str) -> Result<()> {
+	try!(stream.write(line.as_bytes()));
+	match std::old_io::stdio::stdout().write(line.as_bytes()) {
+		Ok(_) => Ok(()),
+		Err(e) => Err(std::io::Error::new(std::io::ErrorKind::Other, e.desc, e.detail)),
+	}
 }
 
 fn main() {
@@ -30,7 +43,7 @@ fn main() {
 	stream.write_fmt(format_args!("{} {}{}\r\n", "USER", nick, " 0 * :tutorial bot"));
 	stream.write_fmt(format_args!("{} {}\r\n", "JOIN", chan));
 
-	let mut buf = [0; 64];
+	let mut buf = [0; 16];
 	let mut bytes_read = stream.read(&mut buf).unwrap();
 	while bytes_read > 0 {
 		print!("{}", match std::str::from_utf8(&buf[..bytes_read]) {
@@ -40,6 +53,7 @@ fn main() {
 				std::str::Utf8Error::TooShort => "Too short",
 			},
 		});
+		print!("$$");
 		bytes_read = stream.read(&mut buf).unwrap();
 	}
 }
