fafdb595075d08565b1347665d7141e579b932b8
Alex Addy <alex.addy@gmail.com>
2015-02-18 22:40:00 -0500

    Changed to old_io use. Now have line reading.

--- net_test.rs
+++ net_test.rs
@@ -1,34 +1,14 @@
-#![feature(net)]
 #![feature(io)]
-#![feature(core)]
-use std::net;
 
-use std::io::Read;
-use std::io::Result;
-use std::io::Write;
+use std::old_io::net::{tcp, addrinfo};
+use std::old_io::{IoResult, BufferedStream};
 
-struct TcpWrapper {
-	stream: net::TcpStream,
-	buf: [u8],
-}
-
-fn start_connection(host: &str, port: u16) -> Result<net::TcpStream> {
-	let mut res = try!(net::lookup_host(host));
-	let first = try!(res.next().unwrap());
-
-	let socket = net::SocketAddr::new(first.ip(), port);
-	let mut stream = try!(net::TcpStream::connect(&socket));
+fn start_connection(host: &str, port: u16) -> IoResult<tcp::TcpStream> {
+	let res = try!(addrinfo::get_host_addresses(host));
+	let mut stream = try!(tcp::TcpStream::connect((res[0], port)));
 	try!(stream.set_nodelay(true));
 
 	return Ok(stream);
-}
-
-fn send_line(mut stream: net::TcpStream, line: &str) -> Result<()> {
-	try!(stream.write(line.as_bytes()));
-	match std::old_io::stdio::stdout().write(line.as_bytes()) {
-		Ok(_) => Ok(()),
-		Err(e) => Err(std::io::Error::new(std::io::ErrorKind::Other, e.desc, e.detail)),
-	}
 }
 
 fn main() {
@@ -37,23 +17,19 @@ fn main() {
 	let chan = "#tutbot-testing";
 	let nick = "tutbot";
 
-	let mut stream = start_connection(server, port).unwrap();
+	let mut stream = BufferedStream::new(start_connection(server, port).unwrap());
 
-	stream.write_fmt(format_args!("{} {}\r\n", "NICK", nick));
-	stream.write_fmt(format_args!("{} {}{}\r\n", "USER", nick, " 0 * :tutorial bot"));
-	stream.write_fmt(format_args!("{} {}\r\n", "JOIN", chan));
+	stream.write_fmt(format_args!("{} {}\r\n", "NICK", nick)).unwrap();
+	stream.flush().unwrap();
+	stream.write_fmt(format_args!("{} {}{}\r\n", "USER", nick, " 0 * :tutorial bot")).unwrap();
+	stream.flush().unwrap();
+	stream.write_fmt(format_args!("{} {}\r\n", "JOIN", chan)).unwrap();
+	stream.flush().unwrap();
 
-	let mut buf = [0; 16];
-	let mut bytes_read = stream.read(&mut buf).unwrap();
-	while bytes_read > 0 {
-		print!("{}", match std::str::from_utf8(&buf[..bytes_read]) {
-			Ok(s) => s,
-			Err(e) => match e {
-				std::str::Utf8Error::InvalidByte(_) => "Invalid Byte",
-				std::str::Utf8Error::TooShort => "Too short",
-			},
-		});
+	let mut result = stream.read_line();
+	while result.is_ok() {
+		print!("{}", result.unwrap());
 		print!("$$");
-		bytes_read = stream.read(&mut buf).unwrap();
+		result = stream.read_line();
 	}
 }
